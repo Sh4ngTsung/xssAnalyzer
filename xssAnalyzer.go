c6673fc335f99f6c9ccb4c031bcdd721bdf90439
Sh4ngTsung <sh4ng@proton.me>
2026-03-01 19:43:50 -0300

    fix: resolve FD leak, prevent timeout deadlocks, and migrate to io.ReadAll

--- xssAnalyzer.go
+++ xssAnalyzer.go
@@ -1,66 +1,74 @@
 package main
 
 import (
-        "bufio"
-        "flag"
-        "fmt"
-        "io/ioutil"
-        "net/http"
-        "os"
-        "strings"
-        "sync"
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
+	"time"
 )
 
 func main() {
-        colorReset := "\033[0m"
-        colorRed := "\033[31m"
-        colorGreen := "\033[32m"
+	colorReset := "\033[0m"
+	colorRed := "\033[31m"
+	colorGreen := "\033[32m"
 
-        // Define a flag "p" to hold the search string
-        searchString := flag.String("p", "", "string to search in response body")
-        flag.Parse()
+	searchString := flag.String("p", "", "")
+	flag.Parse()
 
-        // Ensure that a search string was provided
-        if *searchString == "" {
-                fmt.Println("Error: Please provide a search string using the -p flag")
-                fmt.Println("Usage: xssAnalyzer -p \"<svg onload=confirm(1)>\"")
-                return
-        }
+	if *searchString == "" {
+		fmt.Println("Error: Please provide a search string using the -p flag")
+		fmt.Println("Usage: xssAnalyzer -p \"<svg onload=confirm(1)>\"")
+		return
+	}
 
-        x := bufio.NewScanner(os.Stdin)
+	x := bufio.NewScanner(os.Stdin)
 
-        jobs := make(chan string)
-        var wg sync.WaitGroup
+	jobs := make(chan string)
+	var wg sync.WaitGroup
 
-        for i := 0; i < 30; i++ {
-                wg.Add(1)
-                go func() {
-                        defer wg.Done()
-                        for domain := range jobs {
-                                resp, err := http.Get(domain)
-                                if err != nil {
-                                        continue
-                                }
-                                body, err := ioutil.ReadAll(resp.Body)
-                                if err != nil {
-                                        fmt.Println(err)
-                                }
-                                sb := string(body)
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+	}
 
-                                if strings.Contains(sb, *searchString) {
-                                        fmt.Println(string(colorRed), "XSS FOUND:", domain, string(colorReset))
-                                } else {
-                                        fmt.Println(string(colorGreen), "NOT VULN:", domain, string(colorReset))
-                                }
-                        }
-                }()
-        }
+	for i := 0; i < 30; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for domain := range jobs {
+				func() {
+					resp, err := client.Get(domain)
+					if err != nil {
+						return
+					}
+					defer resp.Body.Close()
 
-        for x.Scan() {
-                domain := x.Text()
-                jobs <- domain
-        }
+					body, err := io.ReadAll(resp.Body)
+					if err != nil {
+						fmt.Println(err)
+						return
+					}
+					sb := string(body)
 
-        close(jobs)
-        wg.Wait()
+					if strings.Contains(sb, *searchString) {
+						fmt.Println(colorRed, "XSS FOUND:", domain, colorReset)
+					} else {
+						fmt.Println(colorGreen, "NOT VULN:", domain, colorReset)
+					}
+				}()
+			}
+		}()
+	}
+
+	for x.Scan() {
+		domain := x.Text()
+		jobs <- domain
+	}
+
+	close(jobs)
+	wg.Wait()
 }
